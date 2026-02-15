254cc004bedd235af2e6b1b3f33886e919fb4cff
FrancesHitchcock <fran.h.hitchcock@gmail.com>
2026-02-15 15:47:08 +0000

    post renders dynamically, likes increment on click

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const posts = [
         location: "Zundert, Netherlands",
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
+        alt: "Self-portrait of Vincent van Gogh frowning.",
         comment: "just took a few mushrooms lol",
         likes: 21
     },
@@ -14,6 +15,7 @@ const posts = [
         location: "Ornans, France",
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
+        alt: "",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
@@ -23,6 +25,7 @@ const posts = [
         location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
+        alt: "",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
@@ -32,4 +35,70 @@ const main = document.getElementById("main");
 
 let innerHtml = ``;
 
+const post = posts[0];
 
+innerHtml += `
+    <section class="user-profile">
+        <div class="user-info">
+            <img class="avatar" src="${post.avatar}" alt="${post.name} avatar">
+            <div class="user-name-wrapper">
+                <h2 class="name">${post.name}</h2>
+                <p class="location">${post.location}</p>
+            </div>
+        </div>
+        <img class="user-img" src="${post.post}" alt="${post.alt}">
+        <div class="profile-section">
+            <div class="icon-container">
+                <img data-postindex="0" class="icon" src="images/icon-heart.png" alt="likes icon.">
+                <img class="icon" src="images/icon-comment.png" alt="comment icon.">
+                <img class="icon" src="images/icon-dm.png" alt="direct message icon.">
+            </div>
+            <p class="like-count"><span class="bold-text">${post.likes} likes</span></p>
+            <p class="username-caption"><span class="bold-text">${post.username}</span> ${post.comment}</p>
+        </div>
+    </section>
+`
+
+function render() {
+
+    let innerHtml = ``;
+
+    const post = posts[0];
+
+    innerHtml += `
+        <section class="user-profile">
+            <div class="user-info">
+                <img class="avatar" src="${post.avatar}" alt="${post.name} avatar">
+                <div class="user-name-wrapper">
+                    <h2 class="name">${post.name}</h2>
+                    <p class="location">${post.location}</p>
+                </div>
+            </div>
+            <img class="user-img" src="${post.post}" alt="${post.alt}">
+            <div class="profile-section">
+                <div class="icon-container">
+                    <img data-postindex="0" class="icon" src="images/icon-heart.png" alt="likes icon.">
+                    <img class="icon" src="images/icon-comment.png" alt="comment icon.">
+                    <img class="icon" src="images/icon-dm.png" alt="direct message icon.">
+                </div>
+                <p class="like-count"><span class="bold-text">${post.likes} likes</span></p>
+                <p class="username-caption"><span class="bold-text">${post.username}</span> ${post.comment}</p>
+            </div>
+        </section>
+    `
+    
+    main.innerHTML = innerHtml;
+}
+
+
+document.addEventListener("click", (e) => {
+    if (e.target.dataset.postindex) {
+
+        const index = Number(e.target.dataset.postindex);
+        
+        posts[index].likes += 1;
+        render();
+    }    
+})
+
+render();
